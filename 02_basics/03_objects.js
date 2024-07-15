fa390fb0191c960b0356ef005e25d4a37314aec8
AsmitAryan001 <152383712+AsmitAryan001@users.noreply.github.com>
2024-07-15 15:06:56 +0000

    object destructure and json intro

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,38 @@
+// singleton
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1", //square brackets are important when declaring a symbol
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser) // locks the value of the object
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`); // string interpolation (used to refer a key inside the object)
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
---
+++ 02_basics/04_objects.js
@@ -0,0 +1,83 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "choudhary"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2} //spread operator used to merge two objects this a better approach than assign operatfor
+// console.log(obj3);
+
+
+const users = [   // these are objects within an array
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+//destructing in objects
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course //here instead of typing course courseInstructor to access it we can use instructor which saves us typing time , this is called destructuring an object
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
