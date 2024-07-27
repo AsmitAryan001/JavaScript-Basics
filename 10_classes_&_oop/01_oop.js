ec1fe7a78d5c5dd2cfc76389d033ad64726221a4
Asmit Aryan <152383712+AsmitAryan001@users.noreply.github.com>
2024-07-27 21:36:42 +0000

    oop

---
+++ 10_classes_&_oop/01_oop.js
@@ -0,0 +1,37 @@
+const user = {
+    username: "Ash",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        //console.log("Got user details from database");
+        // console.log(`Username: ${this.username}`);
+        console.log(this);
+    }
+
+}
+
+
+
+//console.log(user.username)
+//console.log(user.getUserDetails());
+// console.log(this);
+
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;   //this is used here to assign varaibles
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+
+    }
+
+    return this
+}
+
+const userOne = new User("asmit", 12, true) // new is a constructor function which is used to create a new instance for userOne ,if new is not here then userTwo will overwrite its data to userOne as no new instance is being created ,and even if we print userOne the Data of userTTwo will be displayed
+const userTwo = new User("yash", 11, false)
+console.log(userOne.constructor);
+//console.log(userTwo);
