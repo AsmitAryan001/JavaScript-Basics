7202d42f9f9383fa152c2e637ac06e8ba86e7cea
Asmit Aryan <152383712+AsmitAryan001@users.noreply.github.com>
2024-08-17 09:02:17 +0000

    call and this

--- 10_classes_&_oop/04_call.js
+++ 10_classes_&_oop/04_call.js
@@ -5,7 +5,9 @@ function SetUsername(username){
 }
 
 function createUser(username, email, password){
-    SetUsername.call(this, username)
+    SetUsername.call(this, username) // .call is a method that is used to explicitly call a function, it also hold the reference like the variables declared, functions called, values returned and the "this" mentioned here sets the current execution context that now the "this" mentioned here would be used that gives the current context (this passes the context)
+
+    // A formal definition :- .call is used to pass the current execution context from one function to another function
    
     this.email = email
     this.password = password
